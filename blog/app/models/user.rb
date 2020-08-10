e8de52404d97345473cda70bff2f6b695ab55c28
thongnm-1042 <nguyen.minh.thong@sun-asterisk.com>
2020-08-10 16:55:32 +0700

    Chapter 8: Basic login

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  PERMIT_ATTRIBUTES = %i(name email password password_confirmation)
+  PERMIT_ATTRIBUTES = %i(name email password password_confirmation).freeze
   validates :name, presence: true, length: {maximum: Settings.number.max_name}
   validates :email, presence: true,
       length: {maximum: Settings.number.max_email},
