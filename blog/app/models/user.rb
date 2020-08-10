8adf4e285d496c52908e2dff86b9dd19c926b284
thongnm-1042 <nguyen.minh.thong@sun-asterisk.com>
2020-08-10 10:13:14 +0700

    Chapter 6: Modeling users

---
+++ blog/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: Settings.number.max_name}
+  validates :email, presence: true, length: {maximum: Settings.number.max_email},
+      format: {with: Settings.text.string_validate},
+             uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+      length: {minimum: Settings.number.min_pass}
+  has_secure_password
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
