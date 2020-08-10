0a422010f316ce2356e76de7888471c24faa9e65
thongnm-1042 <nguyen.minh.thong@sun-asterisk.com>
2020-08-10 10:27:59 +0700

    Chapter 7: Sign Up

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  PERMIT_ATTRIBUTES = %i(name email password password_confirmation)
   validates :name, presence: true, length: {maximum: Settings.number.max_name}
-  validates :email, presence: true, length: {maximum: Settings.number.max_email},
+  validates :email, presence: true,
+      length: {maximum: Settings.number.max_email},
       format: {with: Settings.text.string_validate},
              uniqueness: {case_sensitive: false}
   validates :password, presence: true,
       length: {minimum: Settings.number.min_pass}
+
   has_secure_password
 
   before_save :downcase_email
