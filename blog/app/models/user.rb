a875108d45ae9ed019787c4545689de1e882ec94
thongnm-1042 <nguyen.minh.thong@sun-asterisk.com>
2020-08-11 11:02:03 +0700

    Chapter 9 : Advance login

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  PERMIT_ATTRIBUTES = %i(name email password password_confirmation)
+  PERMIT_ATTRIBUTES = %i(name email password password_confirmation).freeze
+  attr_accessor :remember_token
+
   validates :name, presence: true, length: {maximum: Settings.number.max_name}
   validates :email, presence: true,
       length: {maximum: Settings.number.max_email},
@@ -9,6 +11,36 @@ class User < ApplicationRecord
       length: {minimum: Settings.number.min_pass}
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
 
   before_save :downcase_email
 
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_081726) do
+ActiveRecord::Schema.define(version: 2020_08_11_011727) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_081726) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
