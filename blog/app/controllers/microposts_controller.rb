0f8be1d6460c6d3d40fed6cc94784b98cf00bf86
thongnm-1042 <nguyen.minh.thong@sun-asterisk.com>
2020-08-14 15:52:23 +0700

    Chapter 13: user microposts

---
+++ blog/app/controllers/microposts_controller.rb
@@ -0,0 +1,46 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: %i(create destroy)
+  before_action :correct_user, only: :destroy
+  before_action :new_micropost, only: :create
+
+  def create
+    @micropost.image.attach micropost_params[:image]
+    if @micropost.save
+      flash[:success] = t "layouts.application.created"
+      redirect_to root_url
+    else
+      @feed_items = current_user.microposts.page(params[:page])
+                              .per Settings.user.per_page
+      flash.now[:danger] = t "layouts.application.created_failed"
+      render "static_pages/home"
+    end
+  end
+
+  def destroy
+    if @micropost.destroy
+      flash[:success] = t "microposts.deleted_success"
+      redirect_to request.referer || root_url
+    else
+      flash.now[:danger] = t "microposts.deleted_error"
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit Micropost::MICROPOST_PARAMS
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by id: params[:id]
+    return if @micropost
+
+    flash[:danger] = t "microposts.deleted_error"
+    redirect_to root_url
+  end
+
+  def new_micropost
+    @micropost = current_user.microposts.build micropost_params
+  end
+end
--- blog/app/javascript/packs/application.js
+++ blog/app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 import 'bootstrap'
+import './micropost'
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
